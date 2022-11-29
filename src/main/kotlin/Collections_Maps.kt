a52d94b98dbb6861aca4aaa74b8d5756b92922c4
Aditya Upadhyay <adiupd123@gmail.com>
2022-11-30 01:01:15 +0530

    Add Maps Program

---
+++ src/main/kotlin/Collections_Maps.kt
@@ -0,0 +1,36 @@
+fun main() {
+    val map1 = mapOf("first" to 1, "second" to 2, "third" to 3, "fourth" to 4)
+    val map2 = mutableMapOf(1 to "A", 2 to "B", 3 to "C", 4 to "D")
+    println(map1)
+    println(map1.keys)
+    println(map1.values)
+    for(item in map1){
+        print("$item ")
+    }
+    println()
+    for(item in map1.keys)
+        print("$item ")
+    println()
+    for(item in map1.values)
+        print("$item ")
+    println()
+    for(item in map1.entries)
+        print("$item ")
+    println()
+    println(map2)
+    map2.put(5, "E")
+    if("A" in map2.values)
+        println("A")
+    if(5 in map2)
+        println(map2[5])
+    map2.forEach{
+        print("$it ")
+    }
+    println()
+    map2.keys.forEach{
+        print("$it ")
+    }
+    println()
+    map2[3] = "F"
+    println(map2)
+}
